0543dc32df6f1dac1475732039deabc04f5ce749
Your Name <ryanhemmila@gmail.com>
2020-01-18 19:28:08 -0600

    Added config

---
+++ config.cpp
@@ -0,0 +1,46 @@
+#include "include/config.h"
+
+/**
+ * Initializes a config object from
+ * a file path
+ */
+Server::Config::Config(char *path) {
+  FILE *file;
+  if((file = fopen(path, "r")) != NULL) {
+    read(file);
+  }
+}
+
+/**
+ * Initializes a config object from
+ * an open file stream
+ */
+Server::Config::Config(FILE *file) {
+  read(file);
+}
+
+/**
+ * Reads all lines from the specified file,
+ * adding in valid entries
+ */
+void Server::Config::read(FILE *file) {
+  int nread;
+  size_t len;
+  char *line, *tok;
+  while((nread = getline(&line, &len, file)) != -1) {
+    if((tok = strtok(line, "=")) != NULL) {
+      char *key = tok;
+      if((tok = strtok(NULL, "=")) != NULL) {
+        entries[key] = tok;
+      }
+    }
+  }
+}
+
+/**
+ * Returns the value associated with
+ * a specific key
+ */
+char *Server::Config::get_value(char *key) {
+  return entries[key];
+}
---
+++ include/config.h
@@ -0,0 +1,31 @@
+#pragma once
+#define CONFIG_H
+
+#include <map>
+
+#include <string.h>
+#include <stdio.h>
+
+namespace Server {
+
+  /**
+   * Represents a collection of key value pairs
+   */
+  class Config {
+    private:
+      struct CompareKey {
+        bool operator()(char const *a, char const *b) const {
+          return strcmp(a, b) < 0;
+        }
+      };
+
+      void read(FILE *file);
+      std::map<char*, char*, CompareKey> entries;
+
+    public:
+      Config(FILE *file);
+      Config(char *path);
+
+      char *get_value(char *key);
+  };
+}
